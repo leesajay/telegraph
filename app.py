dfbc78a89796db0f2d9e11895d8da8b7215c71cb
Molly <mrobison@ischool.berkeley.edu>
2014-04-25 11:51:04 -0700

    template for app.py, will not run because I don't know how to work with how we've been given an instance on the groups server

---
+++ app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from subprocess import check_output
+import flask
+import os
+import sqlite3
+import random
+import string
+from flask import Flask,request, session, escape, redirect, jsonify
+
+# create our little application
+app = Flask(__name__)
+app.debug=True
+
+@app.route('/'):
+	#insert everything here
+	return flask.render_template("index.html")
+
+
+app.secret_key = os.urandom(24)
+
+if __name__ == "__main__":
+	app.run(port=61008)	
