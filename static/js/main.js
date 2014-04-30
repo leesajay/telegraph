e2fbf0be6c3690b6ced4db10a108c63f465ef06f
Molly <mrobison@ischool.berkeley.edu>
2014-04-30 15:00:32 -0700

    gimme a B

--- static/js/main.js
+++ static/js/main.js
@@ -44,8 +44,8 @@ function drawBarGraph(element, data){
 	svg.call(tip);
 
 	//fit the axes to the range of our data
-	x.domain(function(d, i) { return titles[i]; });
-	y.domain([0, d3.max(data, function(d, i) { return dataset[i]; })]);
+	x.domain(titles);
+	y.domain([0, d3.max(dataset[]));
 
 	//put the axes on
 	svg.append("g")
